3757f0ebfb8951488e01940084af98d74efd5749
Jeff Downing <jeff.downing@latchd.com>
2015-05-19 08:23:18 -0400

    Fixed error with 1.8 attempts to run it

--- src/main/java/org/bhn/training/impl/SimpleStringGreeterImpl.java
+++ src/main/java/org/bhn/training/impl/SimpleStringGreeterImpl.java
@@ -3,7 +3,6 @@ package org.bhn.training.impl;
 import org.bhn.training.api.Greeter;
 
 public class SimpleStringGreeterImpl implements Greeter {
-    @Override
     public String greet() {
         return "Hello World!";
     }
